35253c02ec256a8098f17e19903af3649091fe79
Sarthakxtiwari <143182039+Sarthakxtiwari@users.noreply.github.com>
2023-11-02 12:08:19 +0530

    reactStarted
    some progress....

---
+++ webpage/app/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+const data = {
+  nav : {
+    heading: "Hello",
+    subHeading : "Pranshat Singh"
+  },
+  main: {
+    navigationPanel: {
+      h1:"intrests",
+      h2:"acivements"
+    },
+    Posts: ["sarthak","karan"]
+  }
+}
+
+function Nav(){
+  return (
+    <h1>{data.main.navigationPanel.h2}</h1>
+  );
+}
+
+export default function Home() {
+  return (
+    <>sarthak
+     <Nav/>
+    </>
+   
+  )
+}
